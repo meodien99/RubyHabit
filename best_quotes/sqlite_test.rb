9207733d62846aca91c5960d4b7faf797e1ba6d6
meodien99 <seb.madcat@gmail.com>
2015-12-15 13:50:12 +0700

    commit sqlite model

--- best_quotes/sqlite_test.rb
+++ best_quotes/sqlite_test.rb
@@ -9,9 +9,19 @@ end
 STDERR.puts MyTable.schema.inspect
 
 # Create row
-mt = MyTable.create 'title' => 'It happened!',
-                    'posted' => 1,
-                    'body' => 'It did!'
-mt = MyTable.create 'title' => 'I saw it!'
+mt = MyTable.create 'title' => 'I saw it again!'
 
 puts "Count: #{MyTable.count}"
+#
+# top_id = mt["id"].to_i
+# (1..top_id).each do |id|
+#   mt_id = MyTable.find(id)
+#   puts "Found title #{mt_id['title']}"
+# end
+
+mt['title'] = 'I really saved it!'
+mt.save!
+
+mt2 = MyTable.find(mt['id'])
+
+puts "Title: #{mt2['title']}"
